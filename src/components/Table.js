341d5acedcd2eaa56fa0c2a5ac98c47afdbef152
Douglas Araújo <goudxs@gmail.com>
2023-02-04 01:05:45 -0300

    Implementa a lógica para que a tabela seja alimentada pelo estado global

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    const expensesData = expenses.map((expense) => (
+      <tr key={ expense.id }>
+        <td>{expense.description}</td>
+        <td>{expense.tag}</td>
+        <td>{expense.method}</td>
+        <td>{parseFloat(expense.value).toFixed(2)}</td>
+        <td>{expense.exchangeRates[expense.currency].name}</td>
+        <td>{parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
+        <td>
+          {(parseFloat(expense.value)
+            * parseFloat(expense.exchangeRates[expense.currency].ask)).toFixed(2)}
+        </td>
+        <td>Real</td>
+      </tr>
+    ));
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr><th>Descrição</th></tr>
+          <tr><th>Tag</th></tr>
+          <tr><th>Método de pagamento</th></tr>
+          <tr><th>Valor</th></tr>
+          <tr><th>Moeda</th></tr>
+          <tr><th>Câmbio utilizado</th></tr>
+          <tr><th>Valor convertido</th></tr>
+          <tr><th>Moeda de conversão</th></tr>
+          <tr><th>Editar/Excluir</th></tr>
+        </thead>
+        <tbody>{expensesData}</tbody>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = ({ wallet: { expenses } }) => ({
+  expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({})),
+};
+
+Table.defaultProps = {
+  expenses: [],
+};
+
+export default connect(mapStateToProps)(Table);
